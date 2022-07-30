5a03be2fdf7808872a509912770640ed636483ad
Jiho Sohn <jsohn@Jihos-MBP-2.attlocal.net>
2022-07-30 01:56:46 -0400

    updates to JS
    Updates to Readme

--- README.md
+++ README.md
@@ -6,3 +6,19 @@ Simple calculator using JS, HTML, CSS
 
 # ScreenShot
 ![Screen shot of the demo](https://github.com/jsohndata/app-calculator/blob/main/image/-demo.png)
+
+# Known Bug
+- Multiplacation of 0 doesn't work.
+eg) 777 + 88884268889203 * 0
+
+- Equation ending with operator doesn't work.
+777 + 888 + +
+
+- Multiple decimals
+11.1 + 11.1.1.1
+
+- After a calculation it' be nice to reset the display and equation
+
+# Credit
+Thank you @github/devrafalko for the string-math function.
+[See string-math's repo](https://github.com/devrafalko/string-math)
--- scripts/calculator-script.js
+++ scripts/calculator-script.js
@@ -1,68 +1,166 @@
 /* Variables and Functions
 ************************************************** */
-const randomInt    = () => Math.floor(Math.random() * 10000)
+const intMemory = []
+const randomInt = () => Math.floor(Math.random() * 10000)
 
 const addGlobalEventListener = () => {
     document.addEventListener('click', e => {
         if(e.target.matches('button')) {
-            const buttonValue         = e.target.value
-            const buttonDataID        = e.target.dataset.id
-            const display             = document.querySelector('.display')
-            const displayCurrent      = display.innerHTML
-            const displayLength       = display.innerHTML.length
-            const displaySansLastChar = display.innerHTML.slice(0,-1)
-            const renderDisplay       = param => display.innerHTML =  param
-            const renderDisplayPlus   = param => display.innerHTML += param
-            
-            /* Testing Area */
-            // console.log(display.innerHTML.indexOf("."))
+            const buttonValue          = e.target.value
+            const buttonDataID         = e.target.dataset.id
+
+            const display        = document.querySelector('.display')            
+            const displayCurrent = ()  => display.innerHTML
+            const displayLength  = ()  => display.innerHTML.length
+            const displayLastChar = () => displayCurrent().slice(displayLength()-1)
+
+            const equation        = document.querySelector('.display-equation')            
+            const equationCurrent = ()  => equation.innerHTML
+            const equationLength = ()   => equation.innerHTML.length
+            const equationLastChar = () => equationCurrent().slice(equationCurrent()-1)
+            const equationCurrentTrim = () => equation.innerHTML.slice(0,-1)
+
+            const renderDisplay        = param => display.innerHTML = param
+            const renderDisplayPlus    = param => display.innerHTML += param
+            const renderEquation       = param => equation.innerHTML = param
+            const renderEquationPlus   = param => equation.innerHTML += param
 
             switch (buttonDataID) {
                 case 'number':
-                    /* Check if it's initial input */
-                    displayCurrent === "0" ?
-                        renderDisplay(buttonValue) :
-                        renderDisplayPlus(buttonValue)
+                    /* 
+                    Check if there are any number in memory
+                    Avoding: 011 
+                    */
+                    if (intMemory.length === "0") {
+                        renderDisplay(buttonValue)
+                    } else {                  
+                        /*
+                        Before putting in memory (cleaner equation as well) 
+                        make sure to remove the math operator in the first index
+                        Avoding: +500
+                        */
+                        if(parseInt(displayCurrent() )) {                            
+                            renderDisplayPlus(buttonValue)
+                            console.log ("T=>",parseInt(displayCurrent() ))
+                        } else {
+                            renderDisplay(' ')
+                            renderDisplayPlus(buttonValue)                            
+                            console.log ("F=>",parseInt(displayCurrent() ))
+                            console.log ("intMemory=>", intMemory)
+                        }
+                    }
                     break;
                 
+
+
                 case 'decimal':
-                    /* Search string, see if there are other decimal.
-                    Only render one decimal */
-                    displayCurrent.indexOf('.') === -1  ?
-                        renderDisplayPlus(buttonValue) :
-                        null
+                    /* 
+                    Search string, see if there are other decimal.
+                    Only render one decimal, 
+                    Avoiding: 1.....5 
+                    */
+                    if (displayLastChar() !== ".")
+                    {                        
+                        if (displayLastChar() >= 0) {
+                            renderDisplayPlus(buttonValue)
+                        } else {
+                            /* 
+                            if the last char is a math operator 
+                            append a 0 before the period
+                            Avoiding: 7+. 
+                            */
+                            renderDisplayPlus('0.')
+                        }
+                    } else null
                     break;
                 
+
+
                 case 'random': 
-                    /* Check if it's initial input */
-                    displayCurrent === "0" ?
-                        renderDisplay(randomInt()) :
-                        renderDisplayPlus(randomInt())
+                    /* 
+                    If there is 0 on display remove the leading zero.
+                    Avoding: 011 
+                    */
+
+                    /* 
+                    Check if it's initial input
+                    Avoding: 011 
+                    */
+                    if (displayCurrent() === "0") {
+                        renderDisplay(randomInt())
+                    } else {                  
+                        /*
+                        Before putting in memory (cleaner equation as well) 
+                        make sure to remove the math operator in the first index
+                        Avoding: +500
+                        */
+                        if(parseInt(displayCurrent() )) {                            
+                            renderDisplayPlus(randomInt())
+                            console.log ("T=>",parseInt(displayCurrent() ))
+                        } else {
+                            renderDisplay(' ')
+                            renderDisplayPlus(randomInt())                            
+                            console.log ("F=>",parseInt(displayCurrent() ))
+                            console.log ("intMemory=>", randomInt())
+                        }
+                    }
                     break;
 
+
+
                 case 'operator':
-                    /* Check if the last character is an character equal or great than zero.
-                    AKA, not a symbol. If it's a int append a symbol, if it's a symbol null
-                    To avoid duplicate math operators */
-                     (displayCurrent.slice(displayLength-1) >= 0) ?
-                        renderDisplayPlus(buttonValue) :
-                        null
+                    /* 
+                    Check if the last character is not a math operator. 
+                    Append a math operator.
+                    Avoding: 7++++ 
+                    */
+                     if(displayLastChar() >= 0) {
+                        intMemory.push(displayCurrent() )
+                        renderEquationPlus(`${displayCurrent()} ${buttonValue} `)
+                        console.log(intMemory)
+                        renderDisplay(buttonValue)
+                    } else null
+
                     break;                    
                     
+
+
                 case 'clear': 
+                    intMemory.length=0
+                    renderEquation(' ')
                     renderDisplay(0)
                     break;
                 
+
+
                 case 'delete': 
                     renderDisplay(displaySansLastChar)
-                    /* Make sure delete doesn't go down to null. */
-                    displayLength <= 1 ? renderDisplay(0) : null
+                    /* 
+                    Make sure display doesn't go down to null. 
+                    */
+                    displayLength() <= 1 ? renderDisplay(0) : null
                     break;
 
-                case 'process': 
-                    displayCurrent.slice(displayLength-1) >=0 ?
-                        renderDisplay(stringMath(displayCurrent)) :                        
-                        renderDisplay(stringMath(displaySansLastChar))
+
+
+                case 'calculate': 
+                    /* Move the last displayCurrent to the eqaution
+                    to be calculated */
+                    renderEquationPlus(displayCurrent())
+                    
+                    
+
+                    renderDisplay(stringMath(equationCurrent()))
+
+                    
+                    
+                    console.log("intMemory =>", intMemory)
+                    console.log("displayLastChar =>", displayLastChar())    
+                    console.log("equation current =>", equationCurrent())
+                    console.log("equation last char =>", equationLastChar())
+                    console.log("equation trim =>", equationCurrentTrim() )
+                    intMemory.length=0
+                    console.log("intMemory Now =>", intMemory)
                     break;
             }
         }
